021391a1cf941bb1cb88500ddc633a2d7b3a56dd
Ramon-W <77691280+Ramon-W@users.noreply.github.com>
2021-03-04 15:38:40 -0800

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -14,13 +14,8 @@ def render_response():
     pet = request.args['pet']
     symbol = request.args['symbol'] 
     number = request.args['number'] 
-    #The request object stores information about the request sent to the server.
-    #args is an ImmutableMultiDict (like a dictionary but can have mutliple values for the same key and can't be changed)
-    #The information in args is visible in the url for the page being requested. ex. .../response?color=blue
-    if length < 6:
-        reply = "Your password must be longer than 5 characters."
-    if amount == 0;
-        replyTwo = "You must generate at least 1 password."
+    reply = "Your password must be longer than 5 characters."
+    replyTwo = "You must generate at least 1 password."
     return render_template('response.html', response = reply, responseTwo = replyTwo)
     
 if __name__=="__main__":
