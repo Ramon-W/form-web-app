c7d9654b24fcce72901689f9a38413e863e3c1d1
Ramon-W <77691280+Ramon-W@users.noreply.github.com>
2021-03-04 10:03:55 -0800

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -8,7 +8,10 @@ def render_main():
 
 @app.route("/response")
 def render_response():
+    length = request.args['length'] 
+    amount = request.args['amount'] 
     color = request.args['color'] 
+    pet = request.args['pet'] 
     #The request object stores information about the request sent to the server.
     #args is an ImmutableMultiDict (like a dictionary but can have mutliple values for the same key and can't be changed)
     #The information in args is visible in the url for the page being requested. ex. .../response?color=blue
