4986ba80eadd56a33a60f368de228361126976fa
Ramon-W <77691280+Ramon-W@users.noreply.github.com>
2021-03-04 10:16:58 -0800

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -17,10 +17,11 @@ def render_response():
     #The request object stores information about the request sent to the server.
     #args is an ImmutableMultiDict (like a dictionary but can have mutliple values for the same key and can't be changed)
     #The information in args is visible in the url for the page being requested. ex. .../response?color=blue
-    if color == 'pink':
-        reply = "That's my favorite color, too!"
-    else:
-        reply = "My favorite color is pink."
+    if length < 6:
+        reply = "Your password must be longer than 5 characters."
+    elif amount == 0;
+        replyTwo = "You must generate at least 1 password."
+    reply = "My favorite color is pink."
     return render_template('response.html', response = reply)
     
 if __name__=="__main__":
