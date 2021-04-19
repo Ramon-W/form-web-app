c684dc392d1a18dae4c7f8ffa7e5c8b95d30c99c
Ramon-W <77691280+Ramon-W@users.noreply.github.com>
2021-04-19 09:45:40 -0700

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -4,13 +4,13 @@ from flask import Flask, url_for, render_template, request
 
 app = Flask(__name__) #__name__ = "__main__" if this is the file that was run.  Otherwise, it is the name of the file (ex. webapp)
 
-@app.route("/")
+@app.route('/')
 def render_main():
     return render_template('home.html')
 
-@app.route("/response", methods=["GET"]["POST"])
+@app.route('/response', methods=['GET']['POST'])
 def render_response():
-    if request.method == "POST":
+    if request.method == 'POST':
         length = request.form['length'] 
         amount = request.form['amount'] 
         color = request.form['color'] 
